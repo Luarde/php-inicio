f60d69685fb7a656ce0036912066b7c50b05a7b7
Luarde <luarde15@gmail.com>
2020-11-04 20:46:54 -0300

    PHP - Comandos básicos

---
+++ index.php
@@ -0,0 +1,23 @@
+<h1>Primeira Página</h1>
+<?php
+$nome = "Luarde";
+$idade = 24;
+$altura = 1.77;
+$casado = true;
+$alunos = ["João", "Ana", "Paulo"];
+
+if($idade >= 18){
+    echo("<h3>Já pode votar em Biden! </h3>");
+} else {
+    echo("<h3 style='color:red'> Não pode votar ainda</>");
+}
+
+echo("<ul>");
+echo("<li>" . $alunos[0] . "</li>");
+echo("<li>" . $alunos[1] . "</li>");
+echo("<li>" . $alunos[2] . "</li>");
+echo("</ul>");
+
+
+
+?>
